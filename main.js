6ff9df62b71ca641580e0ea64ab2ae876d09a1a1
crist18 <shuzakk@gmail.com>
2014-05-14 16:45:38 +0900

    add define method.

--- main.js
+++ main.js
@@ -7,6 +7,22 @@ enchant();
 define = function() {
 	game.DEBUG = false;
 	game.VERSION = 140514;
+	game.STAGE_ROWS	= 6;
+	game.STAGE_LINES = 5;
+	game.STAGE_H = 250;
+	game.STAGE_W = 300;
+	game.DROP_SIZE = 50;
+	game.DROP_COLOR_SIZE = 6;
+	game.DROP_COLOR_ASSOSIATE = {0:"red", 1:"green", 2:"blue", 3:"yellow", 4:"white", 5:"gray"};
+	game.SLIDE_FRAME = 30;
+	game.SLIDE_EASING = enchant.Easing.CIRC_EASEOUT;
+	game.EXCHANGE_FRAME = 10;
+	game.EXCHANGE_EASING = enchant.Easing.CIRC_EASEOUT;
+	game.GAME_TIME = 120;
+	game.MANIPULATION_TIME = 5;
+	game.MATCH_THRESHOLD = 3;
+	game.BONUS_ADD_PER_ONE = 0.25;
+	game.BONUS_COMBO = 0.25
 }
 
 MainScene = enchant.Class.create(enchant.Scene, {

00e72fe287a2e7bfe68dc46db2dca8089907a9ae
crist18 <shuzakk@gmail.com>
2014-05-15 22:52:39 +0900

    add Drop class.

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ define = function() {
 	game.STAGE_W = 300;
 	game.DROP_SIZE = 50;
 	game.DROP_COLOR_SIZE = 6;
-	game.DROP_COLOR_ASSOSIATE = {0:"red", 1:"green", 2:"blue", 3:"yellow", 4:"white", 5:"gray"};
+	game.DROP_COLOR_ASSOSIATE = {0:"blue", 1:"red", 2:"yellow", 3:"gray", 4:"green", 5:"orange"};
+	game.DROP_COLOR_FRAME = {"blue":0, "red":1, "yellow":2, "gray":3, "green":4, "orange":5};
 	game.SLIDE_FRAME = 30;
 	game.SLIDE_EASING = enchant.Easing.CIRC_EASEOUT;
 	game.EXCHANGE_FRAME = 10;
@@ -25,12 +26,68 @@ define = function() {
 	game.BONUS_COMBO = 0.25
 }
 
+Drop = enchant.Class.create(enchant.Sprite, {
+	initialize: function(width, height) {
+		Sprite.call(this, width, height);
+		this.image = game.assets["images/icon1.png"];
+		this.color = this.getColor();
+		this.frame = this.getFrame();
+		this.row  = 0;
+		this.line = 0;
+		this.matched = false;
+		this.comboChecked = false;
+	}
+	, getColor: function() {
+		var max  = game.DROP_COLOR_SIZE;
+		var list = game.DROP_COLOR_ASSOSIATE;
+		var rand = Math.floor(Math.random() * max);
+		var color = list[rand];
+		if (color) {
+			return color;
+		}
+		return false;
+	}
+	, getFrame: function() {
+		var list = game.DROP_COLOR_FRAME;
+		var color = this.color;
+		if (color) {
+			var frame = list[color];
+			if (frame) {
+				return frame;
+			}
+		}
+		return false;
+	}
+	, setDrop: function(parent, row, line, color) {
+		this.x = row  * game.DROP_SIZE;
+		this.y = line * game.DROP_SIZE;
+		parent.addChild(this);
+	}
+	, move: function(x, y, movetype, params) {
+		var frame  = 10;
+		var easing = enchant.Easing.CIRC_EASEOUT;
+		if (movetype=="slide")	{
+			frame  = game.SLIDE_FRAME;
+			easing = game.SLIDE_EASING;
+		}
+		else if (movetype=="exchenge")	{
+			frame  = game.EXCHANGE_FRAME;
+			easing = game.EXCHANGE_EASING;
+		}
+		this.tl.moveTo(x, y, frame, easing);
+	}
+});
+
 MainScene = enchant.Class.create(enchant.Scene, {
 	initialize: function() {
 		enchant.Scene.call(this);
 		
 		// Main
 		console.log("main game");
+		// Drop
+		var drop01 = new Drop(game.DROP_SIZE, game.DROP_SIZE);
+		this.addChild(drop01);
+		drop01.move(100, 100, "slide");
 
 	}
 });

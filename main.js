49b0012d339a9abc85196c9560ff57f7233530ed
crist18 <shuzakk@gmail.com>
2014-05-18 14:48:08 +0900

    add Dragger class.

--- main.js
+++ main.js
@@ -34,18 +34,21 @@ DropManager = enchant.Class.create({
 			this.drops[i] = new Drop();
 		}
 	}
-	, addStage: function() {
-		this.stage = new Group(game.STAGE_W, game.STAGE_H);
-		this.stage.x = 10;
-		this.stage.y = 60;
-		game.currentScene.addChild(this.stage);
-	}
 	, setDrops: function() {
 		for (var i=0; i<this.drops.length; i++) {
 			var drop = this.drops[i];
 			var row  = Math.floor(i % game.STAGE_ROWS);
 			var line = Math.floor(i / game.STAGE_ROWS);
-			drop.setDrop(this.stage, row, line);
+			drop.setDrop(game.stage, row, line);
+		}
+	}
+	, getDropByRowLine: function(row, line) {
+		for (key in this.drops) {
+			var drop = this.drops[key];
+			if (drop.row == row && drop.line == line) {
+				return drop;
+				break;
+			}
 		}
 	}
 });
@@ -89,8 +92,10 @@ Drop = enchant.Class.create(enchant.Sprite, {
 		return false;
 	}
 	, setDrop: function(parent, row, line, color) {
-		this.x = row * game.DROP_SIZE;
-		this.y = line * game.DROP_SIZE;
+		this.row = row;
+		this.line = line;
+		this.x = this.row * game.DROP_SIZE;
+		this.y = this.line * game.DROP_SIZE;
 		parent.addChild(this);
 	}
 	, move: function(x, y, movetype, params) {
@@ -108,17 +113,74 @@ Drop = enchant.Class.create(enchant.Sprite, {
 	}
 });
 
+/*
+Dragger
+     current
+     selected
+     touchStart()
+     touchMove()
+     touchEnd()
+     exchangeFor(target)
+*/
+Dragger = enchant.Class.create(enchant.Entity, {
+	initialize: function() {
+		Entity.call(this);
+		this.width  = game.STAGE_W;
+		this.height = game.STAGE_H;
+		this.current = null;
+		this.dragging = false;
+	}
+	, ontouchstart: function(e){
+		this.dragging = true;
+		var row  = Math.floor( (e.x - this._offsetX) / game.DROP_SIZE);
+		var line = Math.floor( (e.y - this._offsetY) / game.DROP_SIZE);
+		this.current = game.dropManager.getDropByRowLine(row, line);
+	}
+	, ontouchmove: function(e){
+		this.current.x = e.x - this._offsetX - (game.DROP_SIZE / 2);
+		this.current.y = e.y - this._offsetY - (game.DROP_SIZE / 2);
+		this.current.visible = true;
+	}
+	, ontouchend: function(e){
+		if (this.dragging) {
+			this.dragging = false;
+		}
+	}
+});
+
+
 MainScene = enchant.Class.create(enchant.Scene, {
 	initialize: function() {
 		enchant.Scene.call(this);
 		
 		// Main
 		console.log("main game");
+		// Stage
+		game.stage = new Group(game.STAGE_W, game.STAGE_H);
+		game.stage.x = 10;
+		game.stage.y = 60;
+		this.addChild(game.stage);
 		// DropManager
 		game.dropManager = new DropManager();
-		game.dropManager.addStage();
 		game.dropManager.setDrops();
+		// Dragger
+		game.dragger = new Dragger();
+		game.stage.addChild(game.dragger);
 
+		/*
+		this.sprite = new Sprite(50,50);
+		this.sprite.image = game.assets["images/icon1.png"];
+		this.addChild(this.sprite);
+		this.sprite.addEventListener('touchstart', function() {
+			console.log("test");
+		});
+		*/
+
+		/*
+		this.addEventListener('touchstart', function() {
+			console.log("test");
+		});
+		*/
 	}
 });
 
